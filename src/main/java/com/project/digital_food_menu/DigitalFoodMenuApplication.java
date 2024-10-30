22ef48de2481dbcc27509d1d2fb4841436f2304b
origin <cadorinisrael@gmail.com>
2024-10-29 23:53:39 -0300

    First commit

---
+++ src/main/java/com/project/digital_food_menu/DigitalFoodMenuApplication.java
@@ -0,0 +1,13 @@
+package com.project.digital_food_menu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalFoodMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalFoodMenuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/digital_food_menu/DigitalFoodMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.digital_food_menu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalFoodMenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
