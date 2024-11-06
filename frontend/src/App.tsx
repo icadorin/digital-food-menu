40b08806c34f8fea5864edcd2d2f8a60eab58d6f
origin <cadorinisrael@gmail.com>
2024-11-06 17:37:46 -0300

    Add Card component with CSS styling and integration in App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,15 @@
 import './App.css'
 import { Card } from './components/card/card';
-import { FoodData } from './interface/FoodData';
+import { useFoodData } from './hooks/useFoodData';
 
 function App() {
-  const data: FoodData[] = [];
+  const { data } = useFoodData();
 
   return (
     <div className='container'>
       <h1>Cardápio</h1>
       <div className="card-grid">
-        {data.map(foodData => 
+        {data?.map(foodData => 
           <Card 
             price={foodData.price}
             title={foodData.title}
